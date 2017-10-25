d693477209299a3f1b114294a15d835cc7135c2f
Victor Alessander <alessander@protonmail.com>
2017-10-25 00:23:36 -0300

    First commit

---
+++ src/components/BarChart.vue
@@ -0,0 +1,22 @@
+<script>
+import { Bar, mixins } from 'vue-chartjs'
+
+export default {
+  extends: Bar,
+
+  mixins: [mixins.reactiveProp],
+  props: ['chartData'],
+
+  data () {
+    return {
+      options: {
+        responsive: false
+      }
+    }
+  },
+
+  mounted () {
+    this.renderChart(this.chartData, this.options)
+  }
+}
+</script>
