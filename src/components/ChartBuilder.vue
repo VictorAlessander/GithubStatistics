7ab71a56a46e993382219d0607f3c9bfb6f0da4b
Victor Alessander <alessander@protonmail.com>
2018-04-08 14:01:00 -0300

    Bar chart complete and starting implementation of line chart

--- src/components/ChartBuilder.vue
+++ src/components/ChartBuilder.vue
@@ -1,6 +1,22 @@
 <template>
   <div id="chartbuilder">
-    <barchart :width="300" :heigth="300" v-bind:repository="chartData">
+    <button
+    type="button"
+    name="bar-chart"
+    @click="fetchInfo('bar')">
+    Bar chart
+    </button>
+    <button
+    type="button"
+    name="line-chart"
+    @click="fetchInfo('line')">
+    Line chart
+    </button>
+    <barchart
+    :width="300"
+    :heigth="300"
+    :repository="chartData"
+    v-if="load.bar_chart">
     </barchart>
   </div>
 </template>
@@ -17,6 +33,10 @@ export default {
 
   data () {
     return {
+      load: {
+        bar_chart: false,
+        line_chart: false
+      },
       chartData: {
         labels: [],
         datasets: [{
@@ -30,8 +50,11 @@ export default {
   },
 
   methods: {
-    fetchInfo () {
-      var url = 'https://api.github.com/repos/' + (this.user) + '/' + (this.repositoryname) + '/languages'
+    fetchInfo (chartType) {
+      this.resetState()
+
+      var url = 'https://api.github.com/repos/'
+      url += (this.user) + '/' + (this.repositoryname) + '/languages'
 
       axios({
         url: url,
@@ -42,13 +65,24 @@ export default {
       }).then(response => {
         this.chartData.labels = Object.keys(response.data),
         this.chartData.datasets[0].data = Object.values(response.data)
+
+        switch (chartType) {
+          case 'bar':
+            this.load.bar_chart = true
+            break
+          case 'line':
+            this.load.line_chart = true
+            break
+        }
+
       }).catch(err => {
         console.log('A error 100 was occurred')
       })
     },
 
-    created () {
-      this.fetchInfo()
+    resetState () {
+      this.load.bar_chart = false
+      this.load.line_chart = false
     }
   }
 }
